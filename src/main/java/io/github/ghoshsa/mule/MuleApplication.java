3448216dcd71293cff69922ea3b298cf37262a17
Sayantan Ghosh <ghoshsayantan710@gmail.com>
2026-09-05 23:22:48 +0530

    Initial setup

---
+++ src/main/java/io/github/ghoshsa/mule/MuleApplication.java
@@ -0,0 +1,12 @@
+package io.github.ghoshsa.mule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MuleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MuleApplication.class, args);
+	}
+}
